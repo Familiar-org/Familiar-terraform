9c5a2e2b0a83a3e16ab03751cd8276b8d2a74b46
nowon9159 <nowon9159@gmail.com>
2024-07-08 16:21:20 +0900

    Feat: Write module vpc

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,3 +1,19 @@
-resource "aws_" "name" {
+provider "aws" {
+  region = var.region
+}
 
+module "vpc" {
+  source   = "../../modules/networks/vpc"
+  vpc_cidr = var.vpc_cidr
+  prefix   = var.prefix
+}
+
+module "subnet" {
+  source          = "../../modules/networks/subnet"
+  vpc_cidr        = var.vpc_cidr
+  prefix          = var.prefix
+  pub_subnet_az   = [""]
+  pub_subnet_cidr = [""]
+  pri_subnet_az   = [""]
+  pri_subnet_cidr = [""]
 }
---
+++ environment/dev/terraform.tfvars
@@ -0,0 +1,3 @@
+prefix   = "familiar-dev"
+vpc_cidr = "10.0.0.0/23"
+region = "us-east-1"

e0ffdeb3b264e74913437e39841b00becbd3c120
nowon9159 <nowon9159@gmail.com>
2024-07-08 22:10:21 +0900

    Feat: Public route table

--- modules/networks/route_table/main.tf
+++ modules/networks/route_table/main.tf
@@ -1,53 +1,12 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr
-  tags = {
-    Name = "${var.prefix}-vpc"
-  }
-}
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.pub_subnet_cidr
-  availability_zone       = var.pub_subnet_az
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${prefix}-public-subnet"
-  }
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.pri_subnet_cidr
-  availability_zone       = var.pri_subnet_az
-  tags = {
-    Name = "${prefix}-private-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
+resource "aws_route_table" "route_table_public" {
   vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${prefix}-igw"
-  }
-}
-
-resource "aws_internet_gateway_attachment" "name" {
-  internet_gateway_id = aws_internet_gateway.igw.id
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_route_table" "name" {
-  vpc_id = var.vpc_id
  
   tags = {
     Name = "${var.prefix}-rtb-public"
   }
 }
 
-resource "aws_route" "name" {
-  
-}
-
-resource "aws_route_table_association" "name" {
-  
+resource "aws_route_table_association" "route_table_public_association" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table_public.id
 }

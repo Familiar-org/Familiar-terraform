1cf5c48b335befecbec2b8ad8714ad8f8f88e86d
nowon9159 <nowon9159@gmail.com>
2024-07-08 16:22:57 +0900

    Feat: Write module vpc

---
+++ modules/networks/internet_gateway/main.tf
@@ -0,0 +1,16 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${prefix}-igw"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "igw_attachment" {
+  internet_gateway_id = aws_internet_gateway.igw.id
+  vpc_id              = var.vpc_id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
+
---
+++ modules/networks/subnet/main.tf
@@ -0,0 +1,18 @@
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.pub_subnet_cidr
+  availability_zone       = var.pub_subnet_az
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${prefix}-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.pri_subnet_cidr
+  availability_zone       = var.pri_subnet_az
+  tags = {
+    Name = "${prefix}-private-subnet"
+  }
+}

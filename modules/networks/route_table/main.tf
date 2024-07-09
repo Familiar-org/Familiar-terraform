75232b9e51cbd57677efcd3b70d43fc6981f5f1b
nowon9159 <nowon9159@gmail.com>
2024-07-09 10:56:58 +0900

    Feat: Add public route to igw

--- modules/networks/route_table/main.tf
+++ modules/networks/route_table/main.tf
@@ -1,3 +1,4 @@
+# public rtb
 resource "aws_route_table" "route_table_public" {
   vpc_id = aws_vpc.vpc.id
 
@@ -10,6 +11,16 @@ resource "aws_route_table_association" "route_table_public_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.route_table_public.id
 }
+
+## public route
+
+resource "aws_route" "route_public" {
+  route_table_id         = aws_route_table.route_table_public
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.igw_id
+}
+
+# private rtb
 
 resource "aws_route_table" "route_table_private" {
   vpc_id = aws_vpc.vpc.id
@@ -24,6 +35,8 @@ resource "aws_route_table_association" "route_table_private_association" {
   route_table_id = aws_route_table.route_table_private.id
 }
 
+# private-db rtb
+
 resource "aws_route_table" "route_table_private_db" {
   vpc_id = aws_vpc.vpc.id
 
@@ -36,3 +49,4 @@ resource "aws_route_table_association" "route_table_private_db_association" {
   subnet_id      = aws_subnet.private_db_subnet.id
   route_table_id = aws_route_table.route_table_private_db.id
 }
+

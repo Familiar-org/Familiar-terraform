16f3584a689b42379bd3244adf5b987381900bb1
nowon9159 <nowon9159@gmail.com>
2024-07-11 13:38:44 +0900

    Feat: Writing s3 bucket module, Modify dev subnet module block

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -9,9 +9,10 @@ module "vpc" {
 }
 
 module "subnet" {
-  source                 = "../../modules/networks/subnet"
-  vpc_cidr               = var.vpc_cidr
-  prefix                 = var.prefix
-  pub_subnet_cidr_and_az = var.pub_subnet_cidr_and_az
-  pri_subnet_cidr_and_az = var.pri_subnet_cidr_and_az
+  source                    = "../../modules/networks/subnet"
+  vpc_cidr                  = var.vpc_cidr
+  prefix                    = var.prefix
+  pub_subnet_cidr_and_az    = var.pub_subnet_cidr_and_az
+  pri_subnet_cidr_and_az    = var.pri_subnet_cidr_and_az
+  pri_db_subnet_cidr_and_az = var.pri_db_subnet_cidr_and_az
 }
--- modules/data-stores/s3/main.tf
+++ modules/data-stores/s3/main.tf
@@ -1,1 +1,13 @@
-asdfasdf
+# log bucket
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = var.bucket_name
+  tags = {
+    Name = var.bucket_name
+  }
+}
+
+resource "aws_s3_bucket_acl" "log_bucket_acl" {
+  bucket = aws_s3_bucket.log_bucket.id
+}
+
+# backend bucket

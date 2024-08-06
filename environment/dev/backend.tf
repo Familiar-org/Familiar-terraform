a03868c63e26103cd7414f413d28245cf787e4f6
nowon9159 <nowon9159@gmail.com>
2024-08-06 15:10:04 +0900

    Test: add required_provider

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.56.1"
+    }
+  }
   backend "s3" {
     bucket  = "familiar-terraform-s3-bucket"
     key     = "dev/terraform.tfstate"

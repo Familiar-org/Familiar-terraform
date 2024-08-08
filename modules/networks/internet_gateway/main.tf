25baba3d510aad930e11a5ee948a348be27b2794
nowon9159 <nowon9159@gmail.com>
2024-08-08 14:45:02 +0900

    Fix: igw module associate 제거
    igw module 설정할 때 resource "aws_internet_gateway" 의 경우 자동으로
    associate 되는 것을 몰랐음. 재확인 해보니 option 값으로 설명이 나와
    있었다.
    
    (Optional) The VPC ID to create in. See the
    aws_internet_gateway_attachment resource for an alternate way to attach
    an Internet Gateway to a VPC.

--- modules/networks/internet_gateway/main.tf
+++ modules/networks/internet_gateway/main.tf
@@ -4,8 +4,3 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.prefix}-igw"
   }
 }
-
-resource "aws_internet_gateway_attachment" "igw_attachment" {
-  internet_gateway_id = aws_internet_gateway.igw.id
-  vpc_id              = var.vpc_id
-}

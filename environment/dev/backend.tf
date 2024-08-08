b8219b3422f486b001689528b13017c28fac089e
nowon9159 <nowon9159@gmail.com>
2024-08-08 10:43:29 +0900

    Test: backend local to s3 변경

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -5,13 +5,11 @@ terraform {
       version = "~> 5.56.1"
     }
   }
-  # backend "s3" {
-  #   bucket  = "familiar-terraform-s3-bucket"
-  #   key     = "dev/terraform.tfstate"
-  #   region  = "us-east-1"
-  #   encrypt = true
-  # }
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket  = "familiar-terraform-s3-bucket"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
+
 }

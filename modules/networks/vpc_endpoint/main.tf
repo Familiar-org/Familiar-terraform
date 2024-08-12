9659c39fbd91fba7a923d3a21ed849296497b4aa
nowon9159 <nowon9159@gmail.com>
2024-08-12 14:46:46 +0900

    Feat: Module gw endpoint s3 작성

--- modules/networks/vpc_endpoint/main.tf
+++ modules/networks/vpc_endpoint/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_endpoint" "gw_endpoint_s3" {
-  service_name = "com.amazonaws.us-east-1.s3"
-  vpc_id       = aws_vpc.vpc_id
-
+  service_name    = "com.amazonaws.us-east-1.s3"
+  vpc_id          = var.vpc_id
+  route_table_ids = var.route_table_ids
   tags = {
     Name = "${var.prefix}-gw-endpoint-s3"
   }
--- modules/networks/vpc_endpoint/variables.tf
+++ modules/networks/vpc_endpoint/variables.tf
@@ -1,18 +1,11 @@
 variable "prefix" {
-
+  
 }
-variable "vpc_cidr" {
 
+variable "vpc_id" {
+  
 }
-variable "pub_subnet_cidr" {
 
-}
-variable "pub_subnet_az" {
-
-}
-variable "pri_subnet_cidr" {
-
-}
-variable "pri_subnet_az" {
-
+variable "route_table_ids" {
+  
 }

417db12b2188d2401bbd01e9f59332e912add594
nowon9159 <nowon9159@gmail.com>
2024-08-14 16:04:01 +0900

    Test: route_table 테스트

--- modules/networks/subnet/outputs.tf
+++ modules/networks/subnet/outputs.tf
@@ -1,15 +1,11 @@
-output "pub_subnet_ids" {
-  value = values(aws_subnet.public_subnet)[*].id
+output "pub_subnet_id" {
+  value = aws_subnet.public_subnet
 }
 
 output "pri_subnet_ids" {
-  value = [
-    "${aws_subnet.private_subnet.*.id}"
-  ]
+  value = aws_subnet.private_subnet
 }
 
 output "pri_db_subnet_ids" {
-  value = [
-    "${aws_subnet.private_db_subnet.*.id}"
-  ]
+  value = aws_subnet.private_db_subnet
 }

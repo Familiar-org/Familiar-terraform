963fe10ccc74f34e6785e9a5c4bd345684d42821
nowon9159 <nowon9159@gmail.com>
2024-08-14 15:44:04 +0900

    Test: pub_subnet_id 테스트

--- modules/networks/subnet/outputs.tf
+++ modules/networks/subnet/outputs.tf
@@ -1,17 +1,15 @@
 output "pub_subnet_ids" {
-  value = [
-    "${aws_subnet.public_subnet[each.key].id}"
-  ]
+  value = values(aws_subnet.public_subnet)[*].id
 }
 
 output "pri_subnet_ids" {
   value = [
-    "${aws_subnet.private_subnet[each.key].id}"
+    "${aws_subnet.private_subnet.*.id}"
   ]
 }
 
 output "pri_db_subnet_ids" {
   value = [
-    "${aws_subnet.private_db_subnet[each.key].id}"
+    "${aws_subnet.private_db_subnet.*.id}"
   ]
 }

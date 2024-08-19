2b1fd3ac1a60f9867e98bc39d9379ee9d63bf3a6
nowon9159 <nowon9159@gmail.com>
2024-08-19 15:41:24 +0900

    Feat: Module R53, ACM 결합 및 코드 작성

---
+++ modules/global/domain/main.tf
@@ -0,0 +1,61 @@
+# acm
+
+resource "aws_acm_certificate" "familiar_link" {
+  domain_name       = var.wildcard_familiar_link_domain_name
+  validation_method = "DNS"
+
+  subject_alternative_names = [var.familiar_link_domain_name]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_route53_record" "familiar" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+   # Skips the domain if it doesn't contain a wildcard
+    if length(regexall("\\*\\..+", dvo.domain_name)) > 0
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.familiar.zone_id
+}
+
+
+# Route53
+
+resource "aws_route53_zone" "familiar" {
+  name = var.route53_familiar_zone_name
+}
+
+resource "aws_route53_record" "familiar_a" {
+  zone_id = aws_route53_zone.familiar.id
+  name    = var.familiar_a_name
+  type    = "A"
+
+  alias {
+    name                   = var.familiar_cf_alias_name
+    zone_id                = aws_route53_zone.familiar.id
+    evaluate_target_health = false
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_route53_record" "familiar_www_redirect" {
+  zone_id = aws_route53_zone.familiar.id
+  name    = var.familiar_cname_name
+  type    = "CNAME"
+  records = var.familiar_cname_record
+}
---
+++ modules/global/domain/variables.tf
@@ -0,0 +1,43 @@
+# acm
+
+variable "prefix" {
+  
+}
+
+variable "wildcard_familiar_link_domain_name" {
+  
+}
+variable "familiar_link_domain_name" {
+
+}
+
+variable "familiar_acm_arn" {
+  
+}
+
+# route53
+
+variable "route53_familiar_zone_name" {
+
+}
+
+variable "familiar_a_name" {
+
+}
+
+variable "cf_alias_name" {
+
+}
+
+variable "familiar_cname_name" {
+
+}
+
+variable "familiar_cname_record" {
+
+}
+
+
+variable "familiar_cf_alias_name" {
+
+}

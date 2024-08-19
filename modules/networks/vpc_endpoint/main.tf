1ca0c646d7e51a5ea4765d046664e4e0ebad8af6
nowon9159 <nowon9159@gmail.com>
2024-08-19 13:03:52 +0900

    Feat: Module vpc endpoint policy 작성

--- modules/networks/vpc_endpoint/main.tf
+++ modules/networks/vpc_endpoint/main.tf
@@ -1,7 +1,20 @@
 resource "aws_vpc_endpoint" "gw_endpoint_s3" {
   service_name    = "com.amazonaws.us-east-1.s3"
   vpc_id          = var.vpc_id
-  route_table_ids = var.route_table_ids
+  route_table_ids = [var.route_table_ids]
+  policy = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Action": "*",
+        "Effect": "Allow",
+        "Resource": "*",
+        "Principal": "*"
+      }
+    ]
+  }
+  EOF
   tags = {
     Name = "${var.prefix}-gw-endpoint-s3"
   }

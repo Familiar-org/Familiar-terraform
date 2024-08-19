87d181cf2d800a458d1df7c0f2e451b5ee0b063c
nowon9159 <nowon9159@gmail.com>
2024-08-19 09:22:42 +0900

    Test: Dev subnet 테스트

--- modules/networks/subnet/outputs.tf
+++ modules/networks/subnet/outputs.tf
@@ -1,7 +1,5 @@
 output "pub_subnet_ids" {
-  value = [
-    "${aws_subnet.public_subnet.id}"
-  ]
+  value = { for key, subnet in aws_subnet.public_subnet : key => subnet.id }
 }
 
 output "pri_subnet_ids" {

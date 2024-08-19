ba5a0d13dfd35dd0b232a15d8fe3dd9b01da9512
nowon9159 <nowon9159@gmail.com>
2024-08-19 10:17:32 +0900

    Test: Dev subnet 테스트

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -37,7 +37,7 @@ module "nat_gw" {
   depends_on = [ module.subnet ]
   source        = "../../modules/networks/nat_gateway"
   prefix        = var.prefix
-  pri_subnet_id = module.subnet.pri_subnet_ids["10.0.1.0/26"].id
+  pri_subnet_id = module.subnet.pri_subnet_ids
 }
 
 # module "route_table" {

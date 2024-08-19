2721deebf480d2a0ae1dd1f1bc2f074ec0593758
nowon9159 <nowon9159@gmail.com>
2024-08-19 09:00:17 +0900

    Test: Dev subnet 생성 테스트

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -27,28 +27,28 @@ module "subnet" {
   pri_db_subnet_cidr_and_az = var.pri_db_subnet_cidr_and_az
 }
 
-module "igw" {
-  source = "../../modules/networks/internet_gateway"
-  vpc_id = module.vpc.vpc_id
-  prefix = var.prefix
-}
+# module "igw" {
+#   source = "../../modules/networks/internet_gateway"
+#   vpc_id = module.vpc.vpc_id
+#   prefix = var.prefix
+# }
 
-module "nat_gw" {
-  source        = "../../modules/networks/nat_gateway"
-  prefix        = var.prefix
-  pri_subnet_id = module.subnet.pri_subnet_ids[0].id
-}
+# module "nat_gw" {
+#   source        = "../../modules/networks/nat_gateway"
+#   prefix        = var.prefix
+#   pri_subnet_id = module.subnet.pri_subnet_ids[0].id
+# }
 
-module "route_table" {
-  source                = "../../modules/networks/route_table"
-  prefix                = var.prefix
-  igw_id                = module.igw.igw_id
-  vpc_id                = module.vpc.vpc_id
-  nat_gw_id             = module.nat_gw.nat_gw_id
-  public_subnet_ids     = module.subnet.pub_subnet_ids[0].id
-  private_subnet_ids    = module.subnet.pri_subnet_ids
-  db_private_subnet_ids = module.subnet.pri_db_subnet_ids
-}
+# module "route_table" {
+#   source                = "../../modules/networks/route_table"
+#   prefix                = var.prefix
+#   igw_id                = module.igw.igw_id
+#   vpc_id                = module.vpc.vpc_id
+#   nat_gw_id             = module.nat_gw.nat_gw_id
+#   public_subnet_ids     = module.subnet.pub_subnet_ids[0].id
+#   private_subnet_ids    = module.subnet.pri_subnet_ids
+#   db_private_subnet_ids = module.subnet.pri_db_subnet_ids
+# }
 
 # Front-End
 

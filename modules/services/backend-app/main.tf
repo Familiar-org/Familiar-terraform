05fd6f5a9e3353b8be8ee69be9be541d0855780d
nowon9159 <nowon9159@gmail.com>
2024-08-20 14:32:26 +0900

    Feat: ECS Task role 생성 추가

--- modules/services/backend-app/main.tf
+++ modules/services/backend-app/main.tf
@@ -96,23 +96,52 @@ resource "aws_ecs_capacity_provider" "backend" {
   }
 }
 
-resource "aws_ecs_cluster_capacity_providers" "main" {
-  cluster_name       = aws_ecs_cluster.main.name
-  capacity_providers = [aws_ecs_capacity_provider.main.name]
+resource "aws_ecs_cluster_capacity_providers" "backend_providers" {
+  cluster_name       = aws_ecs_cluster.backend.name
+  capacity_providers = [aws_ecs_capacity_provider.backend.name]
 
   default_capacity_provider_strategy {
-    capacity_provider = aws_ecs_capacity_provider.main.name
+    capacity_provider = aws_ecs_capacity_provider.backend.name
     base              = 1
     weight            = 100
   }
+}
+
+# ECS Task Role
+
+data "aws_iam_policy_document" "ecs_task_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name_prefix        = "${var.prefix}-backend-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
+}
+
+resource "aws_iam_role" "ecs_exec_role" {
+  name_prefix        = "${var.prefix}-backend-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 
 # ECR
 
 resource "aws_ecr_repository" "backend" {
-  name                 = "${var.prefix}-"
-  image_tag_mutability = "IMMUTABLE"
+  name                 = "${var.prefix}-repo-backend"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
 
   image_scanning_configuration {
